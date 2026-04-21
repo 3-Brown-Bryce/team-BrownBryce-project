17ebb580cf7d13c09117d720947ff55e9f01ba7b
vivianshenn <shenv2890@my.sduhsd.net>
2026-04-21 13:19:25 -0700

    added navigation between pages

---
+++ src/Nav.jsx
@@ -0,0 +1,14 @@
+import "./Nav.css"
+function Nav({ setPage }) {
+    return (
+        <div className="nav">
+        <ul>
+            <li onClick={() => setPage("Welcome")}>Welcome</li>
+            <li onClick={() => setPage("Reason")}>Reason</li>
+            <li onClick={() => setPage("Log")}>Log</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
