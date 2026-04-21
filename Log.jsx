119b0348b75931da75605ed430fa6935bd3ca793
Harpy-Eagle47 <messerschmidth4446@my.sduhsd.net>
2026-04-21 13:31:20 -0700

    Added a basic structure to the daily logging page

---
+++ Log.jsx
@@ -0,0 +1,13 @@
+import Journal from './JournalEntry.jsx'
+import Upload from './ImageUpload.jsx';
+
+function DailyLog(){
+    return(
+        <div>
+            <Journal />
+            <Upload />
+        </div>
+    )
+}
+
+export default DailyLog;
