f9e4077e562be7f7fb41d3acb5b0f564f8755da5
Sanmayu1 <nandipatis4941@my.sduhsd.net>
2026-04-27 13:26:24 -0700

    Updating the Css and Jsx of the LoginPage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,12 +1,64 @@
-import GoogleLogin from "./GoogleLogin"
+import "./LoginPage.css";
+import GoogleLogin from "./GoogleLogin";
 
-function LoginPage(){
-    return(
-        <div>
-            <h1>Welcome!</h1>
-            <GoogleLogin />
+
+function LoginPage({ setPage }) {
+  return (
+    <div className="login-container">
+      <div className="login-card">
+
+
+        <div className="header">
+          <h1>Sign Up</h1>
         </div>
-    )
+
+
+        <div className="content">
+
+
+          {/* LEFT SIDE TEXT */}
+          <div className="text-section">
+            <h2>Congratulations</h2>
+            <p>
+              You've just made a huge step on your journey of quitting your addiction
+            </p>
+          </div>
+
+
+          {/* RIGHT SIDE BUTTONS */}
+          <div className="button-section">
+            <button
+              className="primary-btn"
+              onClick={() => setPage("home")}
+            >
+              Get Started
+            </button>
+
+
+            <button
+              className="secondary-btn"
+              onClick={() => setPage("login")}
+            >
+              Login
+            </button>
+          </div>
+
+
+        </div>
+
+
+        {/* Optional Google Login */}
+        <div className="google-login">
+          <GoogleLogin />
+        </div>
+
+
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+
+export default LoginPage;
+
+
