c924a1834bfc9317023313521449cc771915e91e
Sanmayu1 <nandipatis4941@my.sduhsd.net>
2026-04-28 13:36:22 -0700

    fixed jsx of the loginpage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,6 +1,7 @@
 import GoogleLogin from "./GoogleLogin"
 import App from './App.jsx'
 import { useEffect, useState } from "react";
+import "./LoginPage.css"
 
 // Move the state inside the component function
 function LoginPage(){
@@ -12,16 +13,21 @@ function LoginPage(){
 
   return(
     <div>
-      <h1>Welcome!</h1>
+      <h1 className = "Sign_Up">Sign Up!</h1>
+      <div className = "Congradulations">
+      <h1>Congradulations!</h1>
+      <p>You’ve just made a huge step on your journey of quitting your addiction</p>
+      </div>
       <GoogleLogin />
       <button onClick={() => setPage("App")}>
         Guest Login
       </button>
     </div>
+
+
+
   )
 }
 
 
 export default LoginPage;
-
-
