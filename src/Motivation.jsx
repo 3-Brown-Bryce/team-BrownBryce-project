86d09b97bdca4ed6db180c57d1a1f3b03f914def
Harpy-Eagle47 <messerschmidth4446@my.sduhsd.net>
2026-04-28 13:10:54 -0700

    Added an add addiction option in the addiction selection page, and added code that should cause random quotes to generate in the motivation section

--- src/Motivation.jsx
+++ src/Motivation.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+
+const Quotes = [
+    "You're doing great [Name]!",
+    "Keep reaching for the stars [Name]!",
+    "Your [AddictionSelected] does not define you [Name]!",
+    "Recovery takes time [Name]!"
+];
 function Motivation(){
+
+    const [quote, setQuote] = useState(quotes[Math.floor(Math.random() * quotes.length)]);
+
+    const generateQuote = () => {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        setQuote(quotes[randomIndex]);
+    }
     return(
         <div>
-            <h2>You're doing great name!</h2>
+            <h2>{quote}</h2>
+            <button onClick={generateQuote}>Generate new quote</button>
             <p>You got some rewards</p>
             <button>view rewards</button>
         </div>
