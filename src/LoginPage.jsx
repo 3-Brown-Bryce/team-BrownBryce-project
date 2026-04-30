3e3dde4e63bbbb984810f135109118cf93a51625
Harpy-Eagle47 <messerschmidth4446@my.sduhsd.net>
2026-04-30 12:46:46 -0700

    fixed errors in google login and login page

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -2,7 +2,6 @@ import GoogleLogin from "./GoogleLogin"
 import App from './App.jsx'
 import { useEffect, useState } from "react";
 
-<<<<<<< Updated upstream
 // Move the state inside the component function
 function LoginPage(){
   const [page, setPage] = useState("home"); 
@@ -20,19 +19,6 @@ function LoginPage(){
       </button>
     </div>
   )
-=======
-function LoginPage({ setPage, name }) {
-    return(
-        <div>
-            <h1>Welcome!</h1>
-            <GoogleLogin />
-
-             <button onClick={() => setPage("home")}>
-             Back
-         </button>
-     </div>
-  );
->>>>>>> Stashed changes
 }
 
 
