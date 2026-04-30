8752dcd2769f96af17279d0214248f4413f10e78
Ariana Singh <leafwing9@gmail.com>
2026-04-30 12:52:27 -0700

    An attempt at fixing the Guest Login Button, not successful

--- src/GoogleLogin.jsx
+++ src/GoogleLogin.jsx
@@ -4,7 +4,7 @@ import { signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth';
 import { collection, getDocs } from 'firebase/firestore';
 //We have to have a firebase based user collection system in order for the award claiming firebase to work
 //this is part of that code
-import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 
 function GoogleLogin() { 
   
